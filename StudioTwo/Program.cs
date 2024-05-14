b6ae545c743d85d2927a25360394963e05bb53b3
KatieNFerg <ketchersidekatie@gmail.com>
2024-05-14 15:18:59 -0500

    first commit

---
+++ StudioTwo/Program.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+
+string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+Dictionary<char, int> charCounts = [];
+foreach(char letter in loremIpsom.ToUpper().ToCharArray())
+{
+    if(!charCounts.ContainsKey)
+count++;
+}
+foreach(KeyValuePair<char, int>character in characters) {
+    System.Console.WriteLine(character.Key + "(+character.Value+)");
+    count += character.Value;
+    System.Console.WriteLine($"{character.Key}: {character.Value}");
+}
